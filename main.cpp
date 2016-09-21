6925bc7995ffe31a7e87abcec809e641ebb7617e
eukaryo <c3375c15@gmail.com>
2016-09-21 14:44:39 +0900

    [add] const modifier

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ signed main(void) {
 	ios::sync_with_stdio(false);
 
 	REP(a, 1, 1000)REP(b, a + 1, 1000) {
-		int c = 1000 - a - b;
+		const int c = 1000 - a - b;
 		if (a * a + b * b == c * c) {
 			cout << a * b * c << endl;
 			return 0;
